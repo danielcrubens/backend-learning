8d11607b746595b764a3c7db29c6da05870bab09
danielcrubens <dcr.tecnologi@gmail.com>
2025-08-04 18:08:04 -0300

    feat: consumindo os prâmetros de query e route

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ TODO: criando servidor
 const express = require('express');
 const server = express();
 
+//https://localhost:3000/hello?nome=Daniel&idadde=36
+//Query params = ?nome=Daniel&idade=36
 
-server.get('/', (req, res) => {
+server.get('/hello', (req, res) => {
+const {nome, idade}= req.query
+
 return res.json({
   title: 'Express Server',
-  message: 'Hello, World!',
+  message: `Hello,${nome}`,
+  idade: idade
 
 });
 });
 
+//https://localhost:3000/hello/Daniel
+//Route params =/hello/:nome
+ server.get("/hello/:nome",(req, res)=>{
+  const nome = req.params.nome;
+
+  return res.json({
+    title: 'Express Server',
+    message: `Hello,${nome}`,
+  })
+ })
 
 server.listen(3000);
